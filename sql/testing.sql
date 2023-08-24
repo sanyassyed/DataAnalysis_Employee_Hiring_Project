ab66a10f5191d3e9a78d903a7f2a2f3353dd5877
Sanya PC <sanyashireen@gmail.com>
2023-08-24 15:56:51 -0500

    CICD: Dashboard complete

--- sql/testing.sql
+++ sql/testing.sql
@@ -22,3 +22,42 @@ SELECT * FROM employee LIMIT 5;
 SELECT * FROM region LIMIT 5;
 SELECT * FROM businessunit LIMIT 5;
 
+# New Hires in 2011
+SELECT COUNT(*)
+FROM (SELECT EmpID, MAX(date)
+FROM employee
+WHERE TermDate is NULL 
+	  AND YEAR(Date) = 2014
+      AND Year(HireDate) = Year(Date) 
+      AND Month(HireDate) = Month(Date)
+GROUP BY EmpID) as x;
+
+# EmpCount in 2011 terminated included
+SELECT COUNT(*)
+FROM (SELECT EmpID, MAX(date)
+FROM employee
+WHERE YEAR(Date) = 2014
+GROUP BY EmpID) as x;
+
+# Active employees in 2011
+SELECT COUNT(*)
+FROM (SELECT EmpID, MAX(date)
+FROM employee
+WHERE YEAR(Date) = 2014
+      AND TermDate IS NULL
+GROUP BY EmpID) as x;
+
+
+# Terminated Employees
+SELECT COUNT(*)
+FROM (SELECT EmpID, MAX(date)
+FROM employee
+WHERE TermDate is NOT NULL 
+	  AND YEAR(Date) = 2014
+GROUP BY EmpID) as x;
+
+SELECT EmpID, MAX(date) as latest_date
+FROM employee
+WHERE TermDate is NOT NULL 
+	  AND YEAR(Date) = 2014
+GROUP BY EmpID;
