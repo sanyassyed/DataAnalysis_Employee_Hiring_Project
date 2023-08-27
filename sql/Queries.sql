253ffbca44a2cacab3d060b255e1d85a1a0fdc43
Sanya PC <sanyashireen@gmail.com>
2023-08-26 19:51:56 -0500

    CICD: Submission Done

--- sql/Queries.sql
+++ sql/Queries.sql
@@ -10,3 +10,55 @@ ORDER BY 1;
 SELECT DISTINCT(PayTypeID)
 FROM Employee
 ORDER BY 1;
+
+# New Hires in 2014
+SELECT COUNT(*)
+FROM (SELECT EmpID, MAX(date)
+FROM employee
+WHERE TermDate is NULL 
+	  AND YEAR(Date) = 2014
+      AND Year(HireDate) = Year(Date) 
+      AND Month(HireDate) = Month(Date)
+GROUP BY EmpID) as x;
+
+# New Hires yearwise
+SELECT year, COUNT(*) NewHires
+FROM (SELECT YEAR(Date) year, EmpID, MAX(date)
+FROM employee
+WHERE TermDate is NULL 
+      AND Year(HireDate) = Year(Date) 
+      AND Month(HireDate) = Month(Date)
+GROUP BY YEAR(Date), EmpID) as x
+GROUP BY year;
+
+
+# EmpCount in 2014 (terminated included)
+SELECT COUNT(*)
+FROM (SELECT EmpID, MAX(date)
+FROM employee
+WHERE YEAR(Date) = 2014
+GROUP BY EmpID) as x;
+
+# Active employees in 2014
+SELECT COUNT(*)
+FROM (SELECT EmpID, MAX(date)
+FROM employee
+WHERE YEAR(Date) = 2014
+      AND TermDate IS NULL
+GROUP BY EmpID) as x;
+
+
+# Terminated Employees
+SELECT COUNT(*)
+FROM (SELECT EmpID, MAX(date)
+FROM employee
+WHERE TermDate is NOT NULL 
+	  AND YEAR(Date) = 2014
+GROUP BY EmpID) as x;
+
+# List of latest active employee records
+SELECT EmpID, MAX(date) as latest_date
+FROM employee
+WHERE TermDate is NOT NULL 
+	  AND YEAR(Date) = 2014
+GROUP BY EmpID;
